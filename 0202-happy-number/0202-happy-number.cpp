79fb6971be6198374bdbdf2e4b8a06c8689cb158
Vidhi Binani <vidhibinani34@gmail.com>
2026-08-31 10:52:25 +0530

    Time: 0 ms (100%), Space: 8.5 MB (21.52%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isHappy(int n) {
+        unordered_set<int>se;
+        while(n!=1){
+            if(se.find(n)!=se.end()){
+                return false;
+            }
+            se.insert(n);
+            int sum =0;
+            while(n>0){
+                sum+=(n%10)*(n%10);
+                n=n/10;
+            }
+            n=sum;
+        }
+        return true;
+    }
+};
