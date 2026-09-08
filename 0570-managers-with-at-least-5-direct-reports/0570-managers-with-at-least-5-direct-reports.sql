8fedaad40e551de4c430dab9bc290550cb249b7b
Vidhi Binani <vidhibinani34@gmail.com>
2026-09-08 21:40:27 +0530

    Time: 421 ms (27.16%), Space: 0B (100%) - LeetHub

--- 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -1,6 +1,6 @@
-SELECT e2.name
+SELECT e1.name
 FROM Employee e1
 JOIN Employee e2
-ON e1.managerId = e2.id
-GROUP BY e1.managerId, e2.name
-HAVING COUNT(e1.id) >= 5;
+ON e1.id=e2.managerID
+GROUP BY e2.managerId 
+HAVING COUNT(e2.managerId)>=5;
